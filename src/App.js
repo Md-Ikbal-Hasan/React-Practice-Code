3f80b45f52a442deb55ec0e9748a3931acbcd8b0
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-08-02 23:57:42 +0600

    useEffect Done

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
 import React from 'react'
-
 // import './style.css'
 
 // import STATE from './Components/STATE/STATE'
 // import CONDITIONAL_RENDERING from './Components/CONDITIONAL_RENDERING/index'
 // import EVENT_HANDLER_CLASS from './Components/EVENT_HANDLER_CLASS'
 // import EVENT_BINDING from './Components/EVENT_BINDING'
-// import HOOKS_USESTATE1 from './Components/HOOKS_USESTATE/index1'
-// import HOOKS_USESTATE2 from './Components/HOOKS_USESTATE/index2'
+// import HOOKS_USESTATE1 from './Components/HOOKS/useState/index1'
+// import HOOKS_USESTATE2 from './Components/HOOKS/useState/index2'
 // import FORM from './Components/FORM/FORM'
 // import FORM2 from './Components/FORM/FORM2'
 // import Child from './Components/STATE_LIFTING/Child'
 // import Home from './Components/STATE_LIFTING/Home'
 // import Toggle from './Components/Toggle/Toggle'
-import FAQS from './Components/FAQ/FAQS'
+// import FAQS from './Components/FAQ/FAQS'
+import Test from './Components/HOOKS/useEffect/Test'
 
 export default function App() {
-
   // const data = "I am from parent (app) ";
   // // receiving data from child component.....
   // const handleChildData = (childData) => {
   //   console.log("App : ", childData);
 
   // }
-
-
 
   return (
     <div>
@@ -39,7 +36,10 @@ export default function App() {
       {/* <Child data={data} onChildData={handleChildData} /> */}
       {/* <Home />   */}
       {/* <Toggle /> */}
-      <FAQS />
+      {/* <FAQS /> */}
+
+      <Test />
+
 
 
     </div>
---
+++ src/Components/HOOKS/useEffect/Test.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react'
+
+const Test = () => {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        console.log("UseEffect");
+    }, [])
+
+
+
+    return (
+        <div>
+            {console.log("Rendering")}
+            <h1>Count : {count} </h1>
+            <button onClick={() => { setCount((count) => count + 1) }}>+</button>
+        </div>
+    )
+}
+
+export default Test
